2d20627f05a8bca26be6da01edfded11106eca7b
Aryan Sahani <sahaniaryan321@gmail.com>
2025-10-11 19:00:38 +0530

    commit

--- main.py
+++ main.py
@@ -92,7 +92,9 @@ class CommanderAgent:
         6. Tips :
             - Claculator can be used through Keyboard (e.g; using numbers (1,2,3,4,5,6,7,8,9,0), division (/), multiplication (*), addition (+), subtraction (-) and results or equals to (=)).
             - For enabling the searchbar in youtube click '/' then type the search query.
-        
+        7. Remember :
+            - Do not copy or select any text, because you are not allowed to select text rather than read it.
+
         Return a JSON array of steps with format:
         [
             {{
